017fb4ebf04b645988be79e7e547680a36ac716d
Yasasdev <yasaslekamge@gmail.com>
2025-06-05 10:12:15 +0530

    Refactor About, Blog, Contact, Dashboard, and Portfolio pages to use CSS modules for styling; update global styles and add container class for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navbar />
-        {children}
-        <Footer />
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
