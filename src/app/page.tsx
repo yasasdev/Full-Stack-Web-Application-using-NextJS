083b052a18723b8b91870ec3889b39fffa8b15ee
Yasasdev <yasaslekamge@gmail.com>
2025-06-05 14:36:37 +0530

    Home page updated with styles and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import styles from './page.module.css'; 
+import Hero from '/public/images/hero.png';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.description}>
+          Turning your into ideas into reality. We bring together the teams from the global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero image for home" className={styles.img} />
+      </div>
+    </div>
   );
 }
