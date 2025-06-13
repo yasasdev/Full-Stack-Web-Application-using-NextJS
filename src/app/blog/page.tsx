ca9d5f17b190039d580a13012b1bd10081abed06
Yasasdev <yasaslekamge@gmail.com>
2025-06-13 18:36:16 +0530

    Fetch and display blog posts dynamically; refactor Blog component to use async data retrieval

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,58 +3,43 @@ import styles from './page.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    cache: 'no-store', // Disable caching to always fetch fresh data
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+
+  const data = await getData();
+
   return (
     <div className={styles.MainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/images/illustration.png"
-            alt="Blog Image"
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.description}>Description</p>
-        </div>
-      </Link>
-
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/images/illustration.png"
-            alt="Blog Image"
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.description}>Description</p>
-        </div>
-      </Link>
-
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/images/illustration.png"
-            alt="Blog Image"
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.description}>Description</p>
-        </div>
-      </Link>
+      {data.map((item:any) => (
+        <Link href="/blog/testId" className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/illustration.png"
+              alt="Blog Image"
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.description}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Blog
