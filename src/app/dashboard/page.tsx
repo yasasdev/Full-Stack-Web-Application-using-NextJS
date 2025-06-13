f40658c4a35254326bdddd8aa78eac99d9fbc657
Yasasdev <yasaslekamge@gmail.com>
2025-06-13 19:15:01 +0530

    Add SWR for data fetching in Dashboard component and update dependencies

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import styles from './page.module.css'
+"use client"
+
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-  return (
-    <div className={styles.container}>Dashboard</div>
-  )
-}
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-export default Dashboard
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store", // Disable caching to always fetch fresh data
+  //     });
+
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     setData(await res.json());
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+
+  const fetcher = (...args: [RequestInfo, RequestInit?]) => fetch(...args).then(res => res.json());
+
+  const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher);
+
+  console.log(data);
+
+  return <div className={styles.container}>Dashboard</div>;
+};
+
+export default Dashboard;
