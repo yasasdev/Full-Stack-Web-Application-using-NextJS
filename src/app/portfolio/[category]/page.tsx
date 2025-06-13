153e30aef530f1b2780cd30a27b586d3961d11e4
Yasasdev <yasaslekamge@gmail.com>
2025-06-13 15:10:49 +0530

    Add Category page layout and styling; implement item structure with titles and descriptions for improved presentation

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,17 +1,62 @@
 import React from 'react'
 import styles from './page.module.css'
+import Button from '@/components/button/Button'
+import Image from 'next/image'
 
-interface CategoryProps {
-  params: {
-    category: string;
-  };
-}
-
-const Category = ({ params }: CategoryProps) => {
+const Category = ({params}: {params: {category: string}}) => {
   console.log(params)
   return (
     <div className={styles.container}>
-      category
+      <h1 className={styles.categoryTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.description}>Description</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill={true} 
+            src="/images/apps.jpg" 
+            alt="Portfolio Image" 
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.description}>Description</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill={true} 
+            src="/images/apps.jpg" 
+            alt="Portfolio Image" 
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.description}>Description</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill={true} 
+            src="/images/apps.jpg" 
+            alt="Portfolio Image" 
+          />
+        </div>
+      </div>
+
+      
     </div>
   )
 }
