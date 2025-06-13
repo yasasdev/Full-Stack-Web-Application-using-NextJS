e8e504a3820c87f8d9f387a3952e85670a64e19e
Yasasdev <yasaslekamge@gmail.com>
2025-06-13 22:39:50 +0530

    Add mongoose dependency and implement database connection utility

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -2,63 +2,44 @@ import React from 'react'
 import styles from './page.module.css'
 import Button from '@/components/button/Button'
 import Image from 'next/image'
+import { items } from './data.js'
+import { notFound } from 'next/navigation'
+
+const getData = (category: string) => {
+  const data = items[category as keyof typeof items]
+
+  if(data){
+    return data
+  }
+
+  return notFound();
+}
 
 const Category = ({params}: {params: {category: string}}) => {
-  console.log(params)
+  const data = getData(params.category) //Using the word category because the folder name is [category]
   return (
     <div className={styles.container}>
       <h1 className={styles.categoryTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.description}>Description</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-            className={styles.img}
-            fill={true} 
-            src="/images/apps.jpg" 
-            alt="Portfolio Image" 
-          />
-        </div>
-      </div>
 
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.description}>Description</p>
-          <Button text="See More" url="#" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.description}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image 
+              className={styles.img}
+              fill={true} 
+              src={item.image}
+              alt="Portfolio Image" 
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image 
-            className={styles.img}
-            fill={true} 
-            src="/images/apps.jpg" 
-            alt="Portfolio Image" 
-          />
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.description}>Description</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-            className={styles.img}
-            fill={true} 
-            src="/images/apps.jpg" 
-            alt="Portfolio Image" 
-          />
-        </div>
-      </div>
-
-      
+      ))}         
     </div>
-  )
+  );
 }
 
 export default Category
