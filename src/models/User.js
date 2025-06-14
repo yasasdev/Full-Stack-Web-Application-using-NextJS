ed52c5a3c327ed96062e2c2175e2124dc28a1630
Yasasdev <yasaslekamge@gmail.com>
2025-06-14 21:52:25 +0530

    Fix typo in User model schema reference; correct UserSchema to userSchema

--- src/models/User.js
+++ src/models/User.js
@@ -22,6 +22,6 @@ const userSchema = new Schema(
   { timestamps: true } // Automatically manage createdAt and updatedAt fields
 );
 
-const User = mongoose.models.User || mongoose.model("User", UserSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default mongoose.model("User", userSchema);
